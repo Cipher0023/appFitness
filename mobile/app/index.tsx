8181b5315a6fcb1f2d121413319ee91c9678f6ae
Cipher0023 <dani.esparza@hotmail.com>
2025-01-19 10:53:04 -0300

    This commit adds the expo frontend prototype

---
+++ mobile/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View, Image, StyleSheet} from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Image
+        source={require()}
+      />
+
+    </View>
+  );
+}
---
+++ mobile/constants/colors.ts
@@ -0,0 +1,11 @@
+//aqui vão as cores do projeto, mais pra frente compensa olhar de coloca-las como objetos sendo o objeto do modo claro e do modo escuro
+export const colors ={ 
+
+  background: '#0F232C',
+  white: '#FFF',
+  green: '#1EB500',
+  orange: '#F79600',
+  blue: '#009AD6',
+  black: '#000'
+
+};
