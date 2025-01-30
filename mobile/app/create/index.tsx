f6c507970c36178e51b8e3259a648d089e315756
Cipher0023 <dani.esparza@hotmail.com>
2025-01-30 16:14:39 -0300

    This commit may  be the last of this project, i have notice that i was loosing a lot of time building simple components isntead of developing the app itself. i may restart this project, but using a frontend component library

--- mobile/app/create/index.tsx
+++ mobile/app/create/index.tsx
@@ -13,9 +13,29 @@ import { useForm } from 'react-hook-form'
 
 import {colors} from '../../constants/colors'
 import { Header } from '../../components/header';
-import {Input} from '../../components/input/index';
+import {Select} from '../../components/input/select';
+
+const schema = z.object({
+    gender: z.string().min(1, { message: "O sexo é obrigatório"}),
+    objective: z.string().min(1, { message: "O objetivo é obrigatório"}),
+    level: z.string().min(1, { message: "Selecione seu level"}),
+})
+
+type FormData = z.infer<typeof schema>
 
 export default function Create(){
+
+
+    const {control, handleSubmit, formState: {errors, isValid}} = useForm <FormData> ({
+        resolver: zodResolver(schema)
+    })
+
+    const genderOptions= [
+        {label: "Masculino", value: "masculino"},
+        {label: "Feminino", value: "feminino"},
+
+    ]
+    
     return(
         <View style= {styles.basic}>
 
@@ -23,6 +43,20 @@ export default function Create(){
                 step='Passo2'
                 title='Finalizando dieta'
             />
+
+            <ScrollView style={styles.content}>
+
+                <Text style={styles.label}> Sexo:</Text>
+
+                <Select
+                    control={control}
+                    name="gender"
+                    placeholder="selecione o seu sexo.."
+                    error={errors.gender?.message}
+                    options={genderOptions}
+                />
+
+            </ScrollView>
 
         </View>
     );
@@ -32,5 +66,15 @@ const styles = StyleSheet.create({
     basic:{
         backgroundColor: colors.background,
         flex: 1
+    },
+    label:{
+        fontSize:16,
+        fontWeight:'bold',
+        color: colors.white,
+        marginBottom: 8
+    },
+    content:{
+        paddingLeft: 16,
+        paddingRight: 16,
     },
 })
--- mobile/components/input/select.tsx
+++ mobile/components/input/select.tsx
@@ -4,23 +4,26 @@ import{
     Text,
     TouchableOpacity,
     FlatList,
+    Modal
 } from 'react-native';
-
 import {Controller} from 'react-hook-form'
 import {colors} from '../../constants/colors'
 import React from 'react';
+import {Feather} from '@expo/vector-icons'
 
-interface OptionsProps{
-    label: string;
-    value: string|number;
+
+
+
+interface OptionProps{
+    label:string;
+    value:string|number
 }
-
 interface SelectProps{
     name: string;
     control: any;
     placeholder?: string;
     error?: string;
-    options: OptionsProps[]
+    options: OptionProps[]
 }
 
 export function Select({name,control,placeholder,error,options}: SelectProps){
@@ -32,9 +35,42 @@ export function Select({name,control,placeholder,error,options}: SelectProps){
 
                 render={({field:{onChange, onBlur, value }})=>(
                     <>
-                        <TouchableOpacity>
-                            Selecione algo...
+                        <TouchableOpacity style={styles.select}>
+                            <Text>teste</Text>
+                            <Feather name="arrow-down" size={16} collor="#000"/>
                         </TouchableOpacity>
+
+                        <Modal
+                            visible ={true}
+                            animationType="fade"
+                            transparent={true}
+                            onRequestClose={()=> {}}
+                        >
+
+                            <TouchableOpacity 
+                                style={styles.modalContainer}
+                                activeOpacity={1}
+                            >
+                                <TouchableOpacity style={styles.modalContainer}>
+                                    <FlatList
+                                        contentContainerStyle={{ gap: 4}}
+                                        data={options}
+                                        keyExtractor={(item) => item.value.toString()}
+                                        renderItem={({item}) => (
+                                            <TouchableOpacity style={styles.option} >
+                                                <Text>{item.label}</Text>
+                                            </TouchableOpacity>
+                                        )}
+                                    />
+                                    
+                                </TouchableOpacity>
+
+
+                            </TouchableOpacity>
+
+
+
+                        </Modal>
                     </>
                 )}
             />
@@ -59,6 +95,34 @@ const styles =StyleSheet.create({
     errorText:{
         color: 'red',
         marginTop: 4,
+    },
+    select:{
+        flexDirection: 'row',
+        height:44,
+        backgroundColor: colors.white,
+        alignItems: 'center',
+        justifyContent:'space-between',
+        paddingHorizontal:10,
+        borderRadius:10
+
+    },
+    modalContainer:{
+        backgroundColor:'rgba(0,0,0,0.5)',
+        flex:1,
+        justifyContent:'center'
+    },
+    modalContent:{
+        backgroundColor: colors.white,
+        marginHorizontal:10,
+        borderRadius:8,
+        padding:20,
+    },
+    option:{
+        paddingVertical:14,
+        backgroundColor: 'rgba(208,208,208,0.40)',
+        borderRadius: 4,
+        paddingHorizontal:8,
+        
     }
 
 
